e634f4d00bd8a0ddd719a4cf494fc7150ed3509b
Ubuntu <ubuntu@ip-172-31-1-92.eu-west-2.compute.internal>
2021-03-25 10:45:45 +0000

    adding credentials

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 
-
-#install Docker
+sudo apt-get update
 
 curl https://get.docker.com | sudo bash 
+
 sudo usermod -aG docker jenkins
 
-#install Docker-compose
+sudo apt install -y curl jq
+
+version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo $DOCKER_PASSWORD | docker login --username enixon --password-stdin
+
 
